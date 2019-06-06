f369479add70f8c3f2e82bdbf4979302fc377586
AsherBearce <asher.bearce@gmail.com>
2019-06-06 14:34:08 -0600

    Initial commit

---
+++ src/edu/cnm/deepdive/Sorting.java
@@ -0,0 +1,41 @@
+package edu.cnm.deepdive;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Sorting {
+
+  public static void sort(int[] data){
+    if (data.length <= 1){
+      return;
+    }
+
+    int leftSize = data.length / 2;
+    int[] left = Arrays.copyOf(data, leftSize);
+    int[] right = Arrays.copyOfRange(data, leftSize, data.length);
+
+    sort(left);
+    sort(right);
+
+    int leftIndex = 0;
+    int rightIndex = 0;
+    int mergeIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length){
+      if (left[leftIndex] < right[rightIndex]){
+        data[mergeIndex++] = left[leftIndex++];
+      }
+      else{
+        data[mergeIndex++] = right[rightIndex++];
+      }
+    }
+
+    if (leftIndex < left.length){
+      System.arraycopy(left, leftIndex, data, mergeIndex, data.length - mergeIndex);
+    }
+    else{
+      System.arraycopy(right, rightIndex, data, mergeIndex, data.length - mergeIndex);
+    }
+  }
+
+}
